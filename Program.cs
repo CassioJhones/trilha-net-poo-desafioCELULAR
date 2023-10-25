a1f1198fea660a848c605dac80936ddf27bd53ee
Cassio Jhones <cassio.bjhones@gmail.com>
2023-10-25 01:37:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿using DesafioPOO.Models;
+using DESAFIO_CELULAR.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\nSmartphone Iphone:");
+Iphone clt = new Iphone("(19)3546", "M-15", "9999", 120);
+clt.Ligar();
+clt.InstalarAplicativo("HOTMART");
+clt.ReceberLigacao();
+
+Console.WriteLine("\nSmartphone Nokia:");
+Nokia estagiario = new Nokia("(15)3661","c3p0","0000",2);
+estagiario.Ligar();
+estagiario.InstalarAplicativo("TIKTOK");
+estagiario.ReceberLigacao();
